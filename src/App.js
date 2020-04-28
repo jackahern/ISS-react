e9d7820ec23265bbeecd51e24dc92f072fbaf726
Jack Ahern <Jack.ahern@simitive.com>
2020-04-28 15:20:58 +0100

    laying out components and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Content from './components/Content';
 import './App.css';
 
 function App() {
@@ -8,17 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Where is the International Space Station?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="content-bg">
+        <h1>This is the content</h1>
+      </div>
+    )
+  }
+}
+
+export default Content;
