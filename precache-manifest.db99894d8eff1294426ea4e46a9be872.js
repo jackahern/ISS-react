1966c70e2f1a3fcabd502f1d8f1b4d3c3a5214f1
Jack Ahern <Jack.ahern@simitive.com>
2020-04-28 17:03:44 +0100

    Updates

---
+++ precache-manifest.db99894d8eff1294426ea4e46a9be872.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa18faa8d5d631996505c907f41cc64c",
+    "url": "/ISS-react/index.html"
+  },
+  {
+    "revision": "9d904ed01f9b2cd674c9",
+    "url": "/ISS-react/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "a8c7e956a2451c53f3c6",
+    "url": "/ISS-react/static/css/main.64844e77.chunk.css"
+  },
+  {
+    "revision": "9d904ed01f9b2cd674c9",
+    "url": "/ISS-react/static/js/2.13427960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISS-react/static/js/2.13427960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c7e956a2451c53f3c6",
+    "url": "/ISS-react/static/js/main.0b1da3bd.chunk.js"
+  },
+  {
+    "revision": "9c311fe3d9d2d7ad8ea2",
+    "url": "/ISS-react/static/js/runtime-main.2f887112.js"
+  },
+  {
+    "revision": "65bc9c4dd45e3943e3d22b0bda17de57",
+    "url": "/ISS-react/static/media/iss-bg.65bc9c4d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ISS-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
